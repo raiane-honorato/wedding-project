913c76693e2788ddc44d2f2b00d7b3c3d9bc469c
Raiane Honorato <raiane.honorato@ab-inbev.com>
2023-01-07 22:55:52 -0300

    fix: fix header bgi

--- client/src/pages/Home/Header/index.tsx
+++ client/src/pages/Home/Header/index.tsx
@@ -1,12 +1,15 @@
 import { Navbar } from "../../../components/Navbar";
-import imgHeader from "../../../assets/header.png";
+import imgHeader from "../../../assets/header2.png";
 
 export const Header = () => {
   return (
-    <header style={{ backgroundImage: "url('../../../assets/header.png')" }} className='bg-header bg-no-repeat bg-cover bg-[65%] md:bg-center  h-[90vh]'>
+    <header className='relative w-full h-[90vh] overflow-x-hidden'>
+      <div className='absolute h-full w-full overflow-hidden flex items-center justify-center'>
+        <img src={imgHeader} alt='' className='h-full max-w-none lg:w-full lg:h-auto' />
+      </div>
       <Navbar />
-      <div className='absolute left-[10vw] top-32'>
-        <h2 className='font-mainTitle text-brand-white text-8xl md:text-9xl'>{'Raiane & Lucas'}</h2>
+      <div className='absolute left-[10vw] bottom-32'>
+        <h2 className='font-mainTitle text-brand-white text-7xl md:text-9xl'>{'Raiane & Lucas'}</h2>
         <h3 className='font-specialTitle text-brand-white text-3xl md:text-5xl'>22 de abril de 2023</h3>
       </div>
     </header>
