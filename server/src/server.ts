bf91382c45eb9e0c4833e9373c9438a099bcffaa
Raiane Honorato <raiane.honorato@ab-inbev.com>
2023-01-21 09:37:54 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import { routes } from './routes';
+import express from "express";
+import cors from "cors";
+import { routes } from "./routes";
 import { Server } from "socket.io";
 import http from "http";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_PROD_URL,
+  })
+);
 app.use(express.json());
 app.use(routes);
 
@@ -16,14 +20,13 @@ const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
   cors: {
-    origin: "*",
+    origin: process.env.CLIENT_PROD_URL,
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`Usuário conectado no socket ${socket.id}`);
 });
-
 
 serverHttp.listen(process.env.PORT || 3333, () => {
   console.log("http server running");
