60e55f7f096416394829669340448751a3e328b1
Raiane Honorato <raiane.honorato@ab-inbev.com>
2023-03-12 18:14:28 -0300

    feat: adjusts on texts

--- client/src/assets/data/best-people.ts
+++ client/src/assets/data/best-people.ts
@@ -4,30 +4,6 @@ import { bestPeopleImages } from '../best-people';
 export const bestPeople: BestPeople = {
   groom: [
     {
-      name: 'Pedro',
-      description:
-        'Amigo de longuíssima data do noivo, desde antes mesmo de tentar ser pop. Se conheceram fazendo cursinho Pré-Coluni, assim como o Diego, e logo se deram muito bem. Logo foram morar juntos e esta amizade nunca mais deixou de existir. O Doutor Pedro, como gosta de ser chamado, é o mais engraçado, divertido e bobo entre os amigos do noivo, sempre trazendo as piadas mais repetidas possíveis. Será o próximo na fila dos casórios: Hanna, se prepare!',
-      photo: bestPeopleImages.pedro,
-    },
-    {
-      name: 'Diego',
-      description:
-        'O Diego entrou na vida do noivo na época do Pré-Coluni, assim como o Pedro, e desde então os três se tornaram inseparáveis. Moraram juntos em Viçosa, e desde então, provavelmente é o maior conselheiro do novo. Mesmo tendo passado tanto tempo distantes fisicamente um do outro, Diego sempre está ao lado do Lucas para conversar sobre o assunto que vier, e ultimamente, o principal era organizar casamento, já que ele é o mais recente marido da turminha.',
-      photo: bestPeopleImages.diego,
-    },
-    {
-      name: 'Doug',
-      description:
-        'O Lucas sempre diz que o Doug é a pessoa com o melhor coração que já conheceu. Eles já se conectaram desde o primeiro dia de aula na faculdade. Doug logo se tornou membro agregado da Rep. Cevada, e desde então, já teve iniciação alcóolica, muito rolês, caronas no C3, trabalhos de faculdade, jogos de futebol no PS2, cornetadas e companhia pra vida inteira!',
-      photo: bestPeopleImages.doug,
-    },
-    {
-      name: 'Robinho',
-      description:
-        'Robinho é amigo do noivo da época da faculdade, quando fizeram várias matérias juntos e foram quase vizinhos. Porém foi principalmente no fim e depois da faculdade que eles se conectaram ainda mais, quando os estilos de vida e tipos de rolê se alinharam. Ele é extremamente dedicado em tudo que faz, desde corrida ouvindo Daft Punk até na vida profissional, em que se tornou um ponto fora da curva e uma grande admiração do noivo. Ao mesmo tempo, é um grande amigo, sempre disponível, humilde e companheiro, mas sem perder a acidez nos comentários sobre terceiros.',
-      photo: bestPeopleImages.robson,
-    },
-    {
       name: 'Tales e Carla',
       description:
         'O Tales é o irmão do meio do noivo, apesar de parecer ser o mais velho já que é muito maior que ele. Foi com ele que o Lucas aprendeu a dividir as coisas, principalmente a comida. É o melhor companheiro de jogos de tabuleiro e de videogame do mundo, super brincalhão e alto astral o tempo todo. A Carlinha, noiva do Tales, é a cunhada mais fofa que alguém pode ter, sendo sempre muito simpática, divertida e preocupada com o bem-estar de todos. Estão enrolando há quase 10 anos pra casar, mas uma hora esse casamento sai!',
@@ -38,6 +14,30 @@ export const bestPeople: BestPeople = {
       description:
         'Irmão caçula, o Igor parece mal encarado e casca grossa pra quem não conhece. Mas, depois que você o conhece de verdade, vê que ele é uma pessoa doce, sensível e com um coração enorme. Companheiro de rolês em família, jogatinas online e bate-papos sócio-politicos ou de memes até altas horas, o Igor sempre está lá quando o noivo precisa.',
       photo: bestPeopleImages.igor,
+    },
+    {
+      name: 'Pedro',
+      description:
+        'Amigo de longuíssima data do noivo, desde antes mesmo de tentar ser pop. Se conheceram fazendo cursinho Pré-Coluni, assim como o Diego, e logo se deram muito bem. Logo foram morar juntos e esta amizade nunca mais deixou de existir. O Doutor Pedro, como gosta de ser chamado, é o mais engraçado, divertido e bobo entre os amigos do noivo, sempre trazendo as piadas mais repetidas possíveis. Será o próximo na fila dos casórios: Hanna, se prepare!',
+      photo: bestPeopleImages.pedro,
+    },
+    {
+      name: 'Diego',
+      description:
+        'O Diego entrou na vida do noivo na época do Pré-Coluni, assim como o Pedro, e os três se tornaram inseparáveis. Moraram juntos em Viçosa, e, desde então, é o maior conselheiro do novo. Mesmo tendo passado tanto tempo distantes fisicamente um do outro, Diego sempre está ao lado do Lucas para conversar sobre o assunto que vier, e ultimamente, o principal era organizar casamento, já que ele é o mais recente marido da turminha.',
+      photo: bestPeopleImages.diego,
+    },
+    {
+      name: 'Doug',
+      description:
+        'O Lucas sempre diz que o Doug é a pessoa com o melhor coração que já conheceu. Eles já se conectaram desde o primeiro dia de aula na faculdade, quando a amizade começou. Doug logo se tornou membro agregado da Rep. Cevada, e desde então, já teve iniciação alcóolica, muito rolês, caronas no C3, trabalhos de faculdade, jogos de futebol no PS2, cornetadas e companhia pra vida inteira!',
+      photo: bestPeopleImages.doug,
+    },
+    {
+      name: 'Robinho',
+      description:
+        'Robinho é amigo do noivo da época da faculdade, quando fizeram várias matérias juntos e foram quase vizinhos. Porém foi principalmente no fim e depois da faculdade que eles se conectaram ainda mais, quando os estilos de vida e tipos de rolê se alinharam. Ele é extremamente dedicado em tudo que faz, desde corrida ouvindo Daft Punk até na vida profissional, em que se tornou um ponto fora da curva e uma grande admiração do noivo. Ao mesmo tempo, é um grande amigo, sempre disponível, humilde e companheiro, mas sem perder a acidez nos comentários sobre terceiros.',
+      photo: bestPeopleImages.robson,
     },
     {
       name: 'Camila e Rodrigo',
@@ -60,22 +60,16 @@ export const bestPeople: BestPeople = {
       photo: bestPeopleImages.marina,
     },
     {
-      name: 'Tarik',
+      name: 'Bárbara',
       description:
-        'Tarik e Raiane foram a clássica dupla da faculdade, se apoiando nos estudos, nos processos seletivos de estágio, mas também indo em várias festas da facul e acompanhando as sagas dos crushs. Hoje, apesar de morarem longe, a amizade continua firme e forte, e estão sempre conversando sobre livros e fofocas!',
-      photo: bestPeopleImages.tarik,
+        'Bárbara e Raiane se conheceram criancinhas no primário e cresceram juntas. Ao longo dos anos, a amizade foi se fortalecendo através das brincadeiras e noites do pijama na casa uma da outra. Com o tempo, foram trocando as bonecas por festas e se divertiam muito juntas. Hoje em dia, como nenhuma das duas tem disposição pra isso mais, se divertem em casa mesmo hehe. Morando novamente na mesma cidade, se veem com frequência nas noites de pizza da Miriam. É uma grande alegria para o casal ter não só a Babi como grande amiga, mas também toda a sua família, que sempre os acolhem com muita alegria. Além disso, é uma satisfação enorme ver a mais nova integrante da família, a Heleninha, crescendo e ficando cada vez mais a cara do avô Gilson.',
+      photo: bestPeopleImages.barbara,
     },
     {
       name: 'Daniel',
       description:
         'Amigão de infância da noiva, Daniel e Raiane cresceram juntos e formaram uma amizade para a vida toda. Na adolescência, passavam os intervalos da escola na escada jogando em celulares muito modernos e conversando sobre músicas que ninguém mais conhecia. Hoje, voltaram a viver na mesma cidade, e Daniel se aproximou também do noivo, tornando-se o maior companheiro do casal para sair para comer e fugir da quietude de Ouro Branco. Daniel é o amigo mais coração do casal, então se preparem, ele vai chorar muito!',
       photo: bestPeopleImages.daniel,
-    },
-    {
-      name: 'Micael',
-      description:
-        'Micael e Raiane se conheceram na faculdade e se tornaram grandes amigos. Se apoiaram nos estudos, nos processos seletivos de estágio e até fizeram o TCC juntos (só não pergunte para nenhum dos dois sobre o tema, pois já não se lembram rs). Mica também apoiou a vida amorosa da noiva e foi o primeiro promotor desse grande casal. Apesar da distância, continuam grandes amigos e torcendo pelo sucesso um do outro, seja na carreira ou na vida amorosa.',
-      photo: bestPeopleImages.micael,
     },
     {
       name: 'Jéssica',
@@ -86,20 +80,26 @@ export const bestPeople: BestPeople = {
     {
       name: 'Sharon',
       description:
-        'Sharon e Raiane também se conheceram no trabalho e se tornaram grandes amigas. Amiga paulistana de altíssima qualidade, Sharon sempre tem as melhores dicas de rolê, de moda e também de dona de casa também (já ouviram a palavra da mantegueira francesa?).  As ligações ligações visitas vão aos poucos matando as saudades da dupla, que sempre aproveitam todas as oportunidades para falar de trabalho (quem nunca, né?), da vida e do sonho de morarem na mesma cidade outra vez.',
+        'Sharon e Raiane também se conheceram no trabalho e se tornaram grandes amigas. Amiga paulistana de altíssima qualidade, Sharon sempre tem as melhores dicas de rolê, de moda e também de dona de casa (já ouviram a palavra da mantegueira francesa?).  As ligações e visitas vão aos poucos matando as saudades da dupla, que sempre aproveitam todas as oportunidades para falar de trabalho (quem nunca, né?), da vida e do sonho de morarem na mesma cidade outra vez.',
       photo: bestPeopleImages.sharon,
-    },
-    {
-      name: 'Bárbara',
-      description:
-        'Bárbara e Raiane se conheceram criancinhas no primário e cresceram juntas. Ao longo dos anos, a amizade foi se fortalecendo através das brincadeiras e noites do pijama na casa uma da outra. Com o tempo, foram trocando as bonecas por festas e se divertiam muito juntas. Hoje em dia, como nenhuma das duas tem disposição pra isso mais, se divertem em casa mesmo hehe. Morando novamente na mesma cidade, se veem com frequência nas noites de pizza da Miriam. É uma grande alegria para o casal ter não só a Babi como grande amiga, mas também toda a sua família, que sempre os acolhem com muita alegria. Além disso, é uma satisfação enorme ver nova integrante da família, a Heleninha, crescendo, e se tornando cada vez mais a cara do avô Gilson.',
-      photo: bestPeopleImages.barbara,
     },
     {
       name: 'Polly',
       description:
         'Polly (ou Pollyana quando a mãe dela está brava) e Raiane trabalharam juntas por dois anos, nos quais Polly foi sua tutora e chefinha. Aos poucos, o cuidado e carinho que tinham no trabalho uma pela outra foi passando para a vida pessoal. Rai considera Polly sua grande madrinha e conselheira tanto na vida pessoal quanto profissional. Hoje aproveitam todas as oportunidades para se encontrar, seja online ou presencialmente, e trocar sobre trabalho, família e como é bom voltar a viver em Minas após tantos anos de SP.',
       photo: bestPeopleImages.polly,
+    },
+    {
+      name: 'Tarik',
+      description:
+        'Tarik e Raiane foram a clássica dupla da faculdade, se apoiando nos estudos, nos processos seletivos de estágio, mas também indo em várias festas da facul e acompanhando as sagas dos crushs. Hoje, apesar de morarem longe, a amizade continua firme e forte, e estão sempre conversando sobre livros e fofocas!',
+      photo: bestPeopleImages.tarik,
+    },
+    {
+      name: 'Micael',
+      description:
+        'Micael e Raiane se conheceram na faculdade e se tornaram grandes amigos. Se apoiaram nos estudos, nos processos seletivos de estágio e até fizeram o TCC juntos (só não pergunte para nenhum dos dois sobre o tema, pois já não se lembram rs). Mica também apoiou a vida amorosa da noiva e foi o primeiro promotor desse grande casal. Apesar da distância, continuam grandes amigos e torcendo pelo sucesso um do outro, seja na carreira ou na vida amorosa.',
+      photo: bestPeopleImages.micael,
     },
   ],
 };
--- client/src/pages/GodParentsPage/GodParent/index.tsx
+++ client/src/pages/GodParentsPage/GodParent/index.tsx
@@ -4,22 +4,6 @@ interface GodParentProps {
   bestPerson: BestPerson;
 }
 
-// export const GodParent = ({ bestPerson }: GodParentProps) => {
-//   return (
-//     <div className="flex gap-16 items-center">
-//       <img
-//         src={bestPerson.photo}
-//         alt=""
-//         className="w-72 rounded-full border-brand-primary-main border-[6px]"
-//       />
-//       <div>
-//         <h3 className="font-specialTitle text-3xl mb-3">{bestPerson.name}</h3>
-//         <p className="w-[80%]">{bestPerson.description}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
 export const GodParent = ({ bestPerson }: GodParentProps) => {
   return (
     <div className="flex flex-col gap-8 items-center w-full max-w-[520px] p-8 bg-brand-primary_transparent rounded-2xl">
